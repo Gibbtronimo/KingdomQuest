4001247911555abdfe0b71c5e00a419f90e61f1b
Sebastian Hernandez <82076977+Seabass0305@users.noreply.github.com>
2022-11-03 08:00:22 -0500

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -7,177 +7,278 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int d20();
+int d10();
+int d8();
+int d6();
+
+/*
+	Entity Parent Class
+	-------------------
+	The class below acts
+	as a parent class to
+	all entities in our 
+	game. It includes 
+	setter and getter func-
+	tions for variables used
+	by all entities like
+	health, armor class, 
+	attack damage, and 
+	speed.
+*/
+
+
 class Entity         // entity parent class
 {
 private:
-   int health;
-   int armorClass;
-   int attackDam;
-   int speed;
-   string name;
+	int health;
+	int armorClass;
+   	int attackDam;
+   	int speed;
+   	int id;
+   	int level;
+   	int xpScore;
+   	int hitDie;
+   	int xpToGive;
+	int enemyID;
 
 public:
-	Entity()   				// Constructor
-   { health = 0; armorClass = 0; attackDam = 0; speed = 0; name = ''; }
-   ~Entity();  			// Destructor
+	Entity()   				// Default Constructor
+	{ 
+	    health = 0; 
+	    armorClass = 0; 
+	    attackDam = 0; 
+	    speed = 0; 
+	    id = 000; 
+	}
 
-   void setHealth(int);
-   int getHealth()
-   { return health; }
-   void setArmorClass(int);
-   int getArmorClass()
-   { return armorClass; }
-   void setSpeed(int);
-   int getSpeed()
-   { return speed; }
-   void setAttackDamage(int);
-   int getAttackDamage()
-   { return attackDam; }
-   
+    Entity(int entityID)   		// Constructor
+    {
+        switch(entityID) 
+   	    {   
+   	    	case 004:		 // goblin
+   			   	health = 7; 
+    			armorClass = 10; 
+    		    attackDam = d6() + 2; 
+    	    	speed = d20();
+    	    	id = entityID;
+    	    	break;
+
+   		    case 001:
+    	    	health = 24; 
+    	    	armorClass = 12; 
+        		attackDam = d8(); 
+    	    	speed = d20(); 
+    	    	id = entityID;
+   		    	break;
+
+   		    case 002:
+    		    health = 29; 
+    		    armorClass = 10; 
+    		    attackDam = d10(); 
+    		    speed = d20(); 
+    		    id = entityID;
+   			    break;
+   			    
+   			case 003:
+    		    health = 16; 
+    		    armorClass = 9; 
+    	    	attackDam = 2 * d6(); 
+    	    	speed = d20(); 
+    	    	id = entityID;
+   		    	break;
+
+   		    xpScore = 0; 
+    	   	level = 1; 
+    	   	xpToGive = 50; 
+   	    }
+    }
+    void setHealth(int h);
+    int getHealth(); 
+    void setArmorClass(int ac);
+    int getArmorClass();
+    void setSpeed(int s); 
+    int getSpeed(); 
+   	void setAttackDamage(int ad); 
+	int getAttackDamage(); 
+	void takeDamage(int dam); 
+	int getID(); 
+	int rollDamage(int entityID);
+    int getLevel(); 
+    int getScore(); 
+    int getHitDie(); 
+    int getXP();
+	int rollDamage();
 };
 
-class Player : public Entity       // goblin child class
+void Entity::setHealth(int h)
+{ 
+	health = h; 
+}
+
+int Entity::getHealth()
+{ 
+	return health; 
+}
+
+void Entity::setArmorClass(int ac)
+{ 
+	armorClass = ac; 
+}
+
+int Entity::getArmorClass()
 {
-private:
-   int level;
-   int xpScore;
-   int hitDie;
+	return armorClass;
+}
 
-public:
-   Player()             // Constructor
-   { xpToGive = 0; }
-   ~Player();           // Destructor
-   void setLevel();
-   void getLevel()
-   { return level; }
-   void setScore();
-   void getScore()
-   { return xpScore; }
-   void setHitDie();
-   void getHitDie()
-   { return hitDie; }
+void Entity::setSpeed(int s)
+{ 
+	speed = s; 
+}
+        
+int Entity::getSpeed()
+{ 
+	return speed; 
+}
+
+void Entity::setAttackDamage(int ad)
+{ 
+	attackDam = ad; 
+}
+       
+int Entity::getAttackDamage()
+{ 
+	return attackDam; 
+}
+
+void Entity::takeDamage(int dam)
+{ 
+	health -= dam; 
+}
+       
+int Entity::getID()
+{ 
+	return id; 
+}
+
+int Entity::getLevel()
+{ 
+    return level; 
+}
+
+int Entity::getScore()
+{ 
+    return xpScore; 
+}
+
+int Entity::getHitDie()
+{ 
+	return hitDie; 
+}
+
+int Entity::getXP()
+{ 
+	return xpToGive; 	
+}
+
+int Entity::rollDamage()
+{  	
+    return d6() + 2; 
+}
+
+int Entity::rollDamage(int entityID)
+{ 
+   	int damage = 0;
+
+   	switch(entityID) 
+   	{
+  		case 001:
+    		for(int i = 0; i < level; i++)
+    		damage += d8();
+    		return damage;
    
-};
+   		case 002:
+    		for(int i = 0; i < level; i++)
+    		damage += d10();
+    		return damage;
+   				
+   		case 003:
+    		for(int i = 0; i < level*2; i++)
+    		damage += d6();
+    		return damage;
+    				
+    	default:
+    		return 0;
+  	}
+}
 
-class Goblin : public Entity       // goblin child class
+
+int d20()	//one of the die that is used for certain scenarios
 {
-private:
-   int xpToGive;
-   int enemyID;
+	int die;
+	const int min = 1, max = 20;
 
-public:
-   Goblin()             // Constructor
-   { xpToGive = 0; }
+	unsigned seed = time(0);
+	srand(seed);
 
-   ~Goblin();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
+	die = (rand() % (max - min + 1)) + min;
+	
+	//cout << "D20: " << die << endl; // (used this to test if it worked)
+	
+	return die;
+}
 
-class Ogre : public Entity       // ogre child class
+int d10()	//one of the die that is used for certain scenarios
 {
-private:
-   int xpToGive;
-   int enemyID;
+	int die;
+	const int min = 1, max = 10;
 
-public:
-   Ogre()             // Constructor
-   { xpToGive = 0; }
-   ~Ogre();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
+	unsigned seed = time(0);
+	srand(seed);
 
-class Bandit : public Entity       // bandit child class
+	die = (rand() % (max - min + 1)) + min;
+	
+	//cout << "D10: " << die << endl; // (used this to test if it worked)
+	
+	return die;
+}
+
+int d8()	//one of the die that is used for certain scenarios
 {
-private:
-   int xpToGive;
-   int enemyID;
+	int die;
+	const int min = 1, max = 8;
 
-public:
-   Bandit()             // Constructor
-   { xpToGive = 0; }
-   ~Bandit();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
+	unsigned seed = time(0);
+	srand(seed);
 
-class Cultist : public Entity       // cultist child class
+	die = (rand() % (max - min + 1)) + min;
+	
+	//cout << "D8: " << die << endl; // (used this to test if it worked)
+	
+	return die;
+}
+
+int d6()	//one of the die that is used for certain scenarios
 {
-private:
-   int xpToGive;
-   int enemyID;
+	int die;
+	const int min = 1, max = 6;
 
-public:
-   Cultist()             // Constructor
-   { xpToGive = 0; }
-   ~Cultist();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
+	unsigned seed = time(0);
+	srand(seed);
 
-class Beast : public Entity       // harpy child class
-{
-private:
-   int xpToGive;
-   int enemyID;
-
-public:
-   Beast()             // Constructor
-   { xpToGive = 0; }
-   ~Beast();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
-
-class Giant : public Entity       // giant child class
-{
-private:
-   int xpToGive;
-   int enemyID;
-
-public:
-   Giant()             // Constructor
-   { xpToGive = 0; }
-   ~Giant();           // Destructor
-   void getXP()
-   { return xpToGive; }
-
-};
-
-class DeathKight : public Entity       // death knight child class
-{
-private:
-   int xpToGive;
-   int enemyID;
-
-public:
-   DeathKight()             // Constructor
-   { xpToGive = 0; }
-   ~DeathKight();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
-
-class Gilgamesh : public Entity       // gilgamesh child class
-{
-private:
-   int xpToGive;
-   int enemyID;
-
-public:
-   Gilgamesh()             // Constructor
-   { xpToGive = 0; }
-   ~Gilgamesh();           // Destructor
-   void getXP()
-   { return xpToGive; }
-   
-};
+	die = (rand() % (max - min + 1)) + min;
+	
+	//cout << "D6: " << die << endl; // (used this to test if it worked)
+	
+	return die;
+}
 
 #endif
