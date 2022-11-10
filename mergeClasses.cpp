1002860d4f0f69ebc4fdb562d17aa3de4e74f264
jbanales1 <jesusbanales20@gmail.com>
2022-11-10 12:12:07 -0600

    moved a player class function here

--- mergeClasses.cpp
+++ mergeClasses.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "mergeClasses.h"
 
+//-------------------------------------------------------------------------------------------
+//Entity Functions
+//-------------------------------------------------------------------------------------------
+
 void Entity::setHealth(int h){ health = h; }
 
 int Entity::getHealth(){ return health; }
@@ -63,6 +67,128 @@ int Entity::rollDamage()
             return 0;
     }
 }
+
+//-------------------------------------------------------------------------------------------
+//Player Functions
+//-------------------------------------------------------------------------------------------
+
+void Player::nextLocat()
+{
+    string resp;
+    bool invalidIn = true;
+    if (currY < 3)
+    {
+        currY++;
+        cout << "Going to " << getLocat().getName() << endl;
+    }
+    else
+    {
+        currY = 0;
+
+        if (currX == 0) //Forest
+        {
+        while(invalidIn)
+        {
+            cout << "Which Area Would You Like to Go to Next? (1. East Town 2. Castle 3. West Town): ";
+            getline(cin, resp);
+
+            if(resp.length() == 1 && isdigit(resp[0]) && stoi(resp) > 0 && stoi(resp) < 4)
+            {
+                invalidIn = false;
+                if (resp == "1")
+                    currX = 1;
+                else if (resp == "2")
+                    currX = 2;
+                else if (resp == "3")
+                    currX = 3;
+            }
+            else
+            {
+                cout << "User Input is Invalid" << endl;
+            }
+        }
+        }
+        else if (currX == 1) //East Town
+        {
+        while(invalidIn)
+        {
+            cout << "Which Area Would You Like to Go to Next? (1. Forest 2. Castle 3. Mountain): ";
+            getline(cin, resp);
+
+            if(resp.length() == 1 && isdigit(resp[0]) && stoi(resp) > 0 && stoi(resp) < 4)
+            {
+                invalidIn = false;
+                if (resp == "1")
+                    currX = 0;
+                else if (resp == "2")
+                    currX = 2;
+                else if (resp == "3")
+                    currX = 4;
+            }
+            else
+            {
+                cout << "User Input is Invalid" << endl;
+            }
+        }
+        }
+        else if (currX == 2) //Castle
+        {
+            cout << "Final Area is Special, Gotta Code the Dumb Thing to do Special Thing" << endl;
+        }
+        else if (currX == 3) //West Town
+        {
+        while(invalidIn)
+        {
+            cout << "Which Area Would You Like to Go to Next? (1. Forest 2. Castle 3. Mountain): ";
+            getline(cin, resp);
+
+            if(resp.length() == 1 && isdigit(resp[0]) && stoi(resp) > 0 && stoi(resp) < 4)
+            {
+                invalidIn = false;
+                if (resp == "1")
+                    currX = 0;
+                else if (resp == "2")
+                    currX = 2;
+                else if (resp == "3")
+                    currX = 4;
+            }
+            else
+            {
+                cout << "User Input is Invalid" << endl;
+            }
+        }
+        }
+        else if (currX == 4) //Mountain
+        {
+            while(invalidIn)
+            {
+                cout << "Which Area Would You Like to Go to Next? (1. East Town 2. Castle 3. West Town): ";
+                getline(cin, resp);
+
+                if(resp.length() == 1 && isdigit(resp[0]) && stoi(resp) > 0 && stoi(resp) < 4)
+                {
+                    invalidIn = false;
+                    if (resp == "1")
+                        currX = 1;
+                    else if (resp == "2")
+                        currX = 2;
+                    else if (resp == "3")
+                        currX = 3;
+                }
+                else
+                {
+                    cout << "User Input is Invalid" << endl;
+                }
+            }
+        }
+
+        cout << "Going to " << getLocat().getName() << endl;
+    }
+}
+
+//-------------------------------------------------------------------------------------------
+//Regular Functions
+//-------------------------------------------------------------------------------------------
 
 int die(int max)    //one of the die that is used for certain scenarios
 {
